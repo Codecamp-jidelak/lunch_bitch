95d8d74e8a644fa952aa546e55c6879ae139c5ea
Jan Hornych <Jan.Hornych@equabank.cz>
2016-11-10 15:59:32 +0100

    Merger service

--- src/main/java/cz/codecamp/lunchbitch/services/lunchMenuService/LunchMenuService.java
+++ src/main/java/cz/codecamp/lunchbitch/services/lunchMenuService/LunchMenuService.java
@@ -1,12 +1,10 @@
 package cz.codecamp.lunchbitch.services.lunchMenuService;
 
 
-import cz.codecamp.lunchbitch.models.LunchMenu;
 import cz.codecamp.lunchbitch.models.LunchMenuDemand;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Map;
 
 public interface LunchMenuService {
 
@@ -15,6 +13,6 @@ public interface LunchMenuService {
      * @param restaurants id's
      * @param demands to proceed
      */
-    Map<String, LunchMenu> lunchMenuDownload(List<String> restaurants, List<LunchMenuDemand> demands) throws IOException;
+    List<LunchMenuDemand> lunchMenuDownload(List<String> restaurants, List<LunchMenuDemand> demands) throws IOException;
 
 }
