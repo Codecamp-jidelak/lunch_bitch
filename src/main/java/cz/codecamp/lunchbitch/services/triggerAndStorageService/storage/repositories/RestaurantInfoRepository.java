0706e26928c983a6cb92be74d141fd3bad32e3e0
peter-nikodem <peternikodemjr@gmail.com>
2016-11-15 20:15:43 +0100

    Add Postgres access credentials and set PG as a default db. Implement email unsubscribtion. Move entities and repositories package to the top level.

--- src/main/java/cz/codecamp/lunchbitch/services/triggerAndStorageService/storage/repositories/RestaurantInfoRepository.java
+++
@@ -1,7 +0,0 @@
-package cz.codecamp.lunchbitch.services.triggerAndStorageService.storage.repositories;
-
-import cz.codecamp.lunchbitch.services.triggerAndStorageService.storage.entities.RestaurantInfoEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RestaurantInfoRepository extends CrudRepository<RestaurantInfoEntity, String> {
-}
