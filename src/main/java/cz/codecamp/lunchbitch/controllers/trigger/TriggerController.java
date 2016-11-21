6a728dd0face33d65dfaee469bbe2ff1106b13d3
peter-nikodem <peternikodemjr@gmail.com>
2016-11-21 19:34:44 +0100

    Change @RequestBody to @RequestParam in TriggerController.

--- src/main/java/cz/codecamp/lunchbitch/controllers/trigger/TriggerController.java
+++ src/main/java/cz/codecamp/lunchbitch/controllers/trigger/TriggerController.java
@@ -2,10 +2,7 @@ package cz.codecamp.lunchbitch.controllers.trigger;
 
 import cz.codecamp.lunchbitch.services.triggerAndStorageService.LunchMenuSendingTrigger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/trigger")
@@ -15,7 +12,7 @@ public class TriggerController {
     private LunchMenuSendingTrigger trigger;
 
     @RequestMapping(method = RequestMethod.POST)
-    public void trigger(@RequestBody String password) {
+    public void trigger(@RequestParam String password) {
         trigger.triggerSending(password);
     }
 }
